56bbbb5f9bf76be86a40cb2e37bb161e79ceaca0
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-02-05 21:58:23 +0900

    2022_0205 3.1

--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
 import express from "express";
 
+const PORT = 4000;
+
 const app = express();
+
+const handleListening = () =>
+  console.log(`Server linstening on port https://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
