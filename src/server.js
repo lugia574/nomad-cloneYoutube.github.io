803b96816ef117db4137bd3a43a5b79b504df2cf
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-02-08 16:02:11 +0900

    2022_0208 하다가 ssl 보안 오류 뜸 해결중

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("sex");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
-  console.log(`Server linstening on port https://localhost:${PORT}`);
+  console.log(`Server linstening on port https://localhost:${PORT} yo!`);
 
 app.listen(PORT, handleListening);
