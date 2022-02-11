ddad6d01f3534dbf0c5b0df27b5cb65256b1abaa
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-02-11 15:30:49 +0900

    2022_0211 Middleware

--- src/server.js
+++ src/server.js
@@ -4,9 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send("oh");
 };
+
+app.use(logger);
 
 app.get("/", handleHome);
 
