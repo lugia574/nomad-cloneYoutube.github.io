7bed217ec84ae9fb11c5360d7cd0eebc2d6059f6
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-02-13 17:36:57 +0900

    2022_0213 4.0~4.8 Router

---
+++ README.md
@@ -0,0 +1,29 @@
+# Wetube Reloaded
+
+/ -> Home
+
+/join -> Join
+
+/login -> Login
+
+/search -> Search
+
+======================================
+
+/user/:id -> See User
+
+/user/logout -> Log out
+
+/user/edit -> Edit My Profile
+
+/user/delete -> Delete My profile
+
+======================================
+
+/video/:id -> See Video
+
+/video/:id/edit -> Edit Video
+
+/video/:id/delete -> Delete Video
+
+/video/upload -> Upload Video
--- src/server.js
+++ src/server.js
@@ -1,23 +1,19 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res, next) => {
-  console.log("I will respond.");
-  return res.send("home");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
