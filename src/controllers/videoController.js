9681bb175d20d0de93f11139c327b6322f246dc9
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-03-02 21:45:58 +0900

    2022_0302 model

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,62 +1,30 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-  {
-    title: "Fourth Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 4,
-  },
-];
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
+  } catch {
+    return res.render("server-error");
+  }
 };
 export const watch = (req, res) => {
   const { id } = req.params;
   // const id = req.params.id; 랑 같음
-  const video = videos[id - 1];
 
-  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching:` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
   // const id = req.params.id; 랑 같음
-  const video = videos[id - 1];
-  // 수정할 비디오 특정 (id)
 
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing:` });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   //const title = req.body.title;
-
-  videos[id - 1].title = title;
 
   return res.redirect(`/videos/${id}`);
 };
@@ -67,18 +35,6 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-  // here we will add a video to the videos array.
-  const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-
-  videos.push(newVideo);
   return res.redirect("/");
 };
 export const deleteVideo = (req, res) => res.send("Delete Video");
