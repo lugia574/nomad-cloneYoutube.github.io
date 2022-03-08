9c3fe16d1fe570634ce219b67b864f9003a51fe9
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-03-08 21:04:40 +0900

    2022_0308 session

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -12,6 +12,15 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+// 라우터 앞에서 초기화해줘야함
+app.use(
+  session({
+    secret: "hello",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
