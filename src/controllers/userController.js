a8fdd5a7ff75c88123161f47e1477c002678ceb8
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-04-05 16:33:15 +0900

    2022_0405  8.3 Edit Profile POST part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -155,14 +155,40 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl, email: sessionEmail, username: sessionUsername },
     },
     body: { name, email, username, location },
+    file,
   } = req;
+  let searchParam = [];
+  if (sessionEmail !== email) {
+    searchParam.push({ email });
+  }
+  if (sessionUsername !== username) {
+    searchParam.push({ username });
+  }
+  if (searchParam.length > 0) {
+    const foundUser = await User.findOne({ $or: searchParam });
+    if (foundUser && foundUser._id.toString() !== _id) {
+      return res.status(400).render("edit-profile", {
+        pageTitle: "Edit Profile",
+        errorMessage: "This username/email is already taken!!.",
+      });
+    }
+  }
 
-  // const {id}  = req.session.user;
-  // const {name, email, username, location} = req.body;
-  await User.findByIdAndUpdate(_id, { name, email, username, location });
-  return res.render("edit-profile");
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
 };
 export const see = (req, res) => res.send("See");
