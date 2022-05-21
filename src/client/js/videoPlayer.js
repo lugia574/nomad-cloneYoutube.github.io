66b7dc46640c832a2ccd4682a2f0e4618be7cf89
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-05-21 21:19:19 +0900

    2022_0521 videoPlayer Btn function 1

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,27 @@
-console.log("hello");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumen = document.getElementById("volumen");
+
+const handlePlayClick = (e) => {
+  // if the video is playing, pause it
+  if (video.paused) {
+    video.play();
+  }
+  // else play the video
+  else {
+    video.pause();
+  }
+};
+const handlePause = (e) => (playBtn.innerText = "Play");
+const handlePlay = (e) => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {
+  // volumen mute
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
