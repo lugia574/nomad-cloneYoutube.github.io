d468ffa5a4bbb6c93b1516c572fd8a9c6c310b54
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-05-26 17:31:07 +0900

    2022_0526 Recorder Setup

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,14 +1,44 @@
+const { async } = require("regenerator-runtime");
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
+let stream;
+
+const handleStop = async () => {
+  startBtn.innerText = "Start Recorder";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
 const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+  startBtn.innerText = "Stop Recorder";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log("녹화끝남");
+    console.log(event);
+    console.log(event.data);
+  };
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: { width: 300, height: 200 },
   });
   video.srcObject = stream;
   video.play();
-  console.log(stream);
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
