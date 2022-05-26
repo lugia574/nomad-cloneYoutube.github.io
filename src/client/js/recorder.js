190eb86d9b67156ace9b5993c8faf169b7906fe1
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-05-26 16:46:58 +0900

    2022_0526 Recorder Setup

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
+    audio: false,
+    video: { width: 300, height: 200 },
   });
+  video.srcObject = stream;
+  video.play();
   console.log(stream);
 };
 
