67bcd967027de91a90ba951dcf7faaf4031a0fd9
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-05-27 15:46:40 +0900

    2022_0527 webassembly video transcode

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -72,17 +72,10 @@ const handleDownload = async () => {
   actionBtn.addEventListener("click", handleStart);
 };
 
-const handleStop = async () => {
-  actionBtn.innerText = "Download Recorder";
-  actionBtn.removeEventListener("click", handleStop);
-  actionBtn.addEventListener("click", handleDownload);
-
-  recorder.stop();
-};
 const handleStart = async () => {
-  actionBtn.innerText = "Stop Recorder";
+  actionBtn.innerText = "Recording";
+  actionBtn.disabled = true;
   actionBtn.removeEventListener("click", handleStart);
-  actionBtn.addEventListener("click", handleStop);
 
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
@@ -91,15 +84,26 @@ const handleStart = async () => {
     video.src = videoFile;
     video.loop = true;
     video.play();
+
+    actionBtn.innerText = "Download";
+    actionBtn.disabled = false;
+    actionBtn.addEventListener("click", handleDownload);
   };
 
   recorder.start();
+
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
 };
 
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
-    video: { width: 300, height: 200 },
+    video: {
+      width: 1024,
+      height: 576,
+    },
   });
   video.srcObject = stream;
   video.play();
